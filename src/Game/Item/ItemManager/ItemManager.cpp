5a8192acbae55ebdbc99e899e5596366c472b442
ryouchida <soccer.ry05@gmail.com>
2016-05-10 23:45:14 +0900

    ItemManager

--- src/Game/Item/ItemManager/ItemManager.cpp
+++ src/Game/Item/ItemManager/ItemManager.cpp
@@ -2,33 +2,33 @@
 
 ItemManager::ItemManager() {
 
-	//std::string item_file_path = "res/stage/stage"/* + std::to_string(ステージ番号) + */"/item.txt";
-	//std::ifstream item_file(item_file_path);
-	//assert(!item_file.fail());
+	std::string item_file_path = "res/stage/stage10"/* + std::to_string(ステージ番号) + */"/item.txt";
+	std::ifstream item_file(item_file_path);
+	assert(!item_file.fail());
 
-	//while (!item_file.eof())
-	//{
-	//	int item_id;
-	//	Vec2f pos;
-	//	item_file >> item_id;
-	//	item_file >> pos.x();
-	//	item_file >> pos.y();
+	while (!item_file.eof())
+	{
+		int item_id;
+		Vec2f pos;
+		item_file >> item_id;
+		item_file >> pos.x();
+		item_file >> pos.y();
 
-	//	std::function<void()> createItem[] = {
+		std::function<void()> createItem[] = {
 
-	//		[&]() {
+			[&]() {
 
-	//		item_list.emplace_back(new Bomb(pos, Vec2f(100, 100), Vec2f::Zero()));
-	//	},
-	//		[&]() {
+			item_list.emplace_back(new Bomb(pos, Vec2f(100, 100), Vec2f::Zero()));
+		},
+			[&]() {
 
-	//		item_list.emplace_back(new Bow(pos, Vec2f(100, 100)));
-	//	}
-	//	};
+			item_list.emplace_back(new Bow(pos, Vec2f(100, 100)));
+		}
+		};
 
-	//	createItem[item_id]();
+		createItem[item_id]();
 
-	//}
+	}
 }
 
 ItemManager::~ItemManager() {
